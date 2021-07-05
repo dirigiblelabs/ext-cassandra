f54b17fd16f544a1e1892689ee4610fc619437f9
Ivan Volkov <61655573+vanolincikus@users.noreply.github.com>
2021-07-05 11:40:14 +0300

    Update client.js

--- cassandra/client.js
+++ cassandra/client.js
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2010-2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: 2010-2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+/** Client API for  Apache Cassandra */
 exports.getSession = function (host, port) {
     var session = new Session();
     var native = org.eclipse.dirigible.api.cassandra.CassandraFacade.connect(host, port)
